0a9848ced695792d0bcacd7e26c02e6c6cca637e
Karin Ochayon <karinoch070@gmail.com>
2023-07-24 09:21:59 +0300

    Working on the socket

--- src/pages/CodeBlockPage.jsx
+++ src/pages/CodeBlockPage.jsx
@@ -3,51 +3,61 @@ import { useParams } from "react-router-dom";
 import io from "socket.io-client";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/material.css"; // Replace 'material' with your desired theme
+import "codemirror/theme/material.css";
 import "codemirror/mode/javascript/javascript";
+
 
 const socket = io("http://localhost:3001");
 
 function CodeBlockPage() {
   const { codeBlockTitle } = useParams();
   const [codeBlock, setCodeBlock] = useState({});
-
+  const [isMentor, setIsMentor] = useState(null);
+  
+  console.log({isMentor})
+  
   useEffect(() => {
     const getCodeBlock = async () => {
-      // Fetch the code block data from the server based on the selected title
       try {
         const url = `http://localhost:3001/codeblockpage/${encodeURIComponent(
           codeBlockTitle
-        )}`;
-        const response = await fetch(url);
-        const responseToJson = await response.json()
-
-        setCodeBlock(responseToJson);
-      } catch (err) {
-        //
+          )}`;
+          const response = await fetch(url);
+          const responseToJson = await response.json();
+          
+          setCodeBlock(responseToJson);
+        } catch (err) {
+          console.error("Error fetching code block:", err);
       }
     };
-
+    
     getCodeBlock();
+
 
     socket.on("codeBlockUpdated", (updatedCodeBlock) => {
       setCodeBlock(updatedCodeBlock);
     });
 
+    socket.emit("mentorStatus");
+
+    socket.on("getMentorStatus", (isMentor) => {
+      setIsMentor(isMentor);
+    });
+
     return () => {
       socket.off("codeBlockUpdated");
+      socket.off("mentorStatus");
     };
   }, [codeBlockTitle]);
 
   const handleCodeChange = (editor, data, value) => {
-    // Update the state with the edited code
-    setCodeBlock((prevCodeBlock) => ({ ...prevCodeBlock, code: value }));
-
-    // Emit the codeBlockChange event to notify the backend of the code change
-    socket.emit("codeBlockChange", { title: codeBlock.title, code: value });
+    if (!isMentor) {
+      setCodeBlock((prevCodeBlock) => ({ ...prevCodeBlock, code: value }));
+      socket.emit("codeBlockChange", { title: codeBlock.title, code: value });
+    }
   };
 
-  if (!codeBlock) {
+  if (!codeBlock.title) {
     return <div>Loading...</div>;
   }
 
@@ -57,7 +67,7 @@ function CodeBlockPage() {
       <pre>
         <CodeMirror
           value={codeBlock.code}
-          onBeforeChange={handleCodeChange}
+          onBeforeChange={(editor, data, value) => handleCodeChange(editor, data, value)}
           options={{
             mode: "javascript",
             theme: "material",
@@ -65,6 +75,7 @@ function CodeBlockPage() {
             indentUnit: 2,
             tabSize: 2,
             autofocus: true,
+            readOnly: isMentor,
           }}
         />
       </pre>
